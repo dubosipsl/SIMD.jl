cfd21fa860fc96de8b996cc6e1368bf3d5817849
Thomas Dubos <prenom.nom@lmd.ipsl.fr>
2023-10-30 13:24:46 +0100

    Tighten tolerance when testing SLEEFPirates

--- test/test_SLEEF_Ext.jl
+++ test/test_SLEEF_Ext.jl
@@ -1,12 +1,18 @@
 using SLEEFPirates
-
-@testset "SIMD+SLEEFPirates" begin
-    include("test_SIMD.jl")
-end
-
 const SLEEF_Ext = Base.get_extension(SIMD, :SLEEF_Ext)
-data(F, N, ::Function) = range(F(0.1), F(0.9), length = N)
+
+# @testset "SIMD+SLEEFPirates" begin
+#     include("test_SIMD.jl")
+# end
+
+data(F, N, ::Function) = range(F(0.01), F(0.9), length = N)
 data(F, N, ::typeof(acosh)) = range(F(1.1), F(1.9), length = N)
+
+tolerance(op) = 1
+tolerance(::typeof(@fastmath log))=2
+tolerance(::typeof(@fastmath tanh))=2
+tolerance(::typeof(exp))=2
+tolerance(::typeof(exp10))=2
 
 for (mod, unops) in (
     (Base, SLEEF_Ext.unops_Base_SP),
@@ -14,14 +20,16 @@ for (mod, unops) in (
     (Base.FastMath, SLEEF_Ext.unops_FM_SP_slow))
     for unop in unops
         op = getfield(mod, unop)
+        tol = tolerance(op)
         @testset "$(string(mod)).$(string(op))" begin
             for F in (Float32, Float64), N in (4, 8, 16, 32)
                 d = data(F, N, op)
-                ref = SIMD.Vec(map(op, d)...)
                 arg = SIMD.Vec(d...)
+                ref = SIMD.Vec(map(op, d)...)
                 res = op(arg)
-                if any(abs(res - ref) > 4eps(F))
-                    @info op arg ref res abs(ref - res)
+                err = abs(res - ref)/abs(ref)
+                if any( err > tol*eps(F))
+                    @warn op arg ref res err
                     @test false
                 else
                     @test true
